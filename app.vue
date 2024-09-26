dfeae5d62c6ce9ecd18a505578bd15e77d8adb71
mortalcr <daporrasn@gmail.com>
2024-09-26 13:29:51 -0600

    xd

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-[100vh] w-full bg-slate-800">
+  <div class="h-[85.8vh] w-full bg-slate-800">
     <!-- Navbar -->
     <nav class="navbar">
       <ul class="navbar-menu">
@@ -20,10 +20,24 @@
         </li>
       </ul>
     </nav>
-
+    
     <!-- Contenido de la página -->
     <NuxtPage/>
   </div>
+  <footer
+            class="w-full min-h-[100px] flex flex-row justify-around bg-slate-600 shadow-[0_1px_3px_2px_rgba(0,0,0,0.2)]">
+            <section class="flex flex-col items-start my-5">
+                <div>
+                    <h1 class="font-bold text-2xl flex-col">Developers</h1>
+                </div>
+                <div class="w-fit flex flex- gap-4">
+                    <Author name="Aaron González" github="https://github.com/zSnails"
+                        linked="https://www.linkedin.com/in/aaron-gonz%C3%A1lez-araya-2b48b223a" />
+                    <Author name="Daniel Porras" github="https://github.com/mortalcr"
+                        linked="https://www.linkedin.com/in/mortalcr" />
+                </div>
+            </section>
+      </footer>
 </template>
 
 <script setup>
@@ -32,7 +46,6 @@
 <style scoped>
 /* Estilos para la navbar */
 .navbar {
-  background-color: #1e1b29; /* Fondo oscuro */
   padding: 1rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
@@ -59,5 +72,9 @@
 
 .navbar-button:hover {
   background-color: #fb923c; /* Naranja claro para hover */
+}
+
+.footer{
+  bottom:0px;
 }
 </style>
